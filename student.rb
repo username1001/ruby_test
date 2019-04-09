b379d13d44a3443e3dbe1b769d2c605cd422642d
M <faymatt96@gmail.com>
2019-04-08 20:45:01 -0400

    Add argument

--- student.rb
+++ student.rb
@@ -1,11 +1,11 @@
 class Student
   attr_accessor :first_name, :last_name, :primary_phone_number
 
-  def introduction
-    puts "Hi, I'm #{first_name}!"
+  def introduction(target)
+    puts "Hi #{target}, I'm #{first_name}!"
   end
 end
 
 frank = Student.new
 frank.first_name = "Frank"
-frank.introduction
+frank.introduction('Katrina')
